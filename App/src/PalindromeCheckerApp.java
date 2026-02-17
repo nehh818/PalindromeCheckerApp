e47e43598235fdd19bf336392c33a7dfd4621649
nehh818 <nd3208@srmist.edu.in>
2026-02-17 15:43:38 +0530

    (Neha) added UC2-HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -23,7 +23,7 @@ public class PalindromeCheckerApp{
         String word = "madam";
         String reverse = "";
 
-        for(int i = word.length()-1; i >=0 ; i--){
+        for(int i = word.length()-1; i >=0 ;i--){
             reverse += word.charAt(i);
         }
         if (word.equals(reverse)) {
